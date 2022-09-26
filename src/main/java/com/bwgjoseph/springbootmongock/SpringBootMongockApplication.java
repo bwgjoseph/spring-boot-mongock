2c3ce03ded722c5c5a05e3fed627feb9b02e5753
bwgjoseph <josephgan@live.com.sg>
2022-09-27 00:19:49 +0800

    Initial Commit

---
+++ src/main/java/com/bwgjoseph/springbootmongock/SpringBootMongockApplication.java
@@ -0,0 +1,15 @@
+package com.bwgjoseph.springbootmongock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.mongock.runner.springboot.EnableMongock;
+
+@SpringBootApplication
+public class SpringBootMongockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwgjoseph/springbootmongock/SpringBootMongockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwgjoseph.springbootmongock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
